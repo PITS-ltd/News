9cdd4db4ac9f99e2175682fb4b13ad68b69dcfcd
Илья <54807032+ElMKV@users.noreply.github.com>
2023-06-14 18:59:25 +0500

    add search, appbar, balance

--- news/lib/futures/main/main.dart
+++ news/lib/futures/main/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news/core/globals/globals.dart';
-import 'package:news/data/model/news.dart';
-import 'package:news/futures/core_widgets/card_news.dart';
-import 'package:news/futures/main/bloc/news_bloc.dart';
-import 'package:news/futures/web_view_news/web_view_container.dart';
+import 'package:badges/badges.dart' as badges;
 
 void main() {
   runApp(const MyApp());
@@ -17,119 +12,163 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      scaffoldMessengerKey: snackbarKey, // <= this
-
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: NewsPage(),
+      home: MainPage(),
     );
   }
 }
 
-class NewsPage extends StatelessWidget {
-  NewsPage({super.key});
+class MainPage extends StatelessWidget {
+  MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => NewsBloc()..add(NewsGet()),
-      child: BlocConsumer<NewsBloc, NewsState>(
-        listener: (context, state) {
-          // TODO: implement listener
-        },
-        builder: (context, state) {
-          if (state is NewsLoadingState) {
-            return const Scaffold(
-                body: Center(child: CircularProgressIndicator()));
-          } else if (state is NewsError) {
-            return Scaffold(
-              body: Center(child: Text(state.pageState.error)),
-            );
-          }
-          News? news = state.pageState.news;
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text("News"),
-              actions: <Widget>[
-                Padding(
-                    padding: const EdgeInsets.only(right: 20.0),
-                    child: IconButton(
-                      onPressed: () =>
-                          context.read<NewsBloc>().add(NewsMakeAllReadEvent()),
-                      icon: const Icon(Icons.checklist_outlined),
-                    )),
-              ],
-            ),
-            body: Center(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                        padding: const EdgeInsets.all(8),
-                        itemCount: news?.articles.length,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: CardNews(
-                                  imgUrl: news?.articles[index].urlToImage,
-                                  title: news?.articles[index].title));
-                        }),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                        padding: const EdgeInsets.all(8),
-                        itemCount: news?.articles.length,
-                        scrollDirection: Axis.vertical,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Center(
-                              child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: GestureDetector(
-                              onTap: () {
-                                if (news?.articles[index].url?.isNotEmpty ??
-                                    false) {
-                                  context
-                                      .read<NewsBloc>()
-                                      .add(NewsMakeOneReadEvent(index));
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => WebViewExample(
-                                              url: news!.articles[index].url!,
-                                            )),
-                                  );
-                                }
-                              },
-                              child: Card(
-                                child: ListTile(
-                                  title: Text(
-                                    news?.articles[index].title ?? '' ,style: TextStyle(fontSize: 12),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  trailing: state.pageState.isRead[index]
-                                      ? const Icon(
-                                          Icons.done_outlined,
-                                          color: Colors.green,
-                                        )
-                                      : const Icon(
-                                          Icons.notification_important_outlined,
-                                          color: Colors.red,
-                                        ),
-                                ),
-                              ),
-                            ),
-                          ));
-                        }),
-                  )
-                ],
-              ),
-            ),
-          );
-        },
+    // return Container(
+    //   color: Colors.red,
+    //   // decoration: const BoxDecoration(
+    //   //     gradient: LinearGradient(
+    //   //       begin: Alignment.topCenter,
+    //   //       end: Alignment(1, 0.001),
+    //   //       colors: <Color>[
+    //   //         Color(0xff009df5),
+    //   //         Color(0xff027fc6),
+    //   //       ],
+    //   //       // Gradient from https://learnui.design/tools/gradient-generator.html
+    //   //       // tileMode: TileMode.mirror,
+    //   //     )),
+    //   child: Scaffold(
+
+    //       body: _body()),
+    // );
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('Вася'),
+        leading: const Padding(
+          padding: EdgeInsets.all(2.0),
+          child: CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: 24,
+              child: CircleAvatar(
+                radius: 24.0,
+                backgroundImage: NetworkImage(
+                    "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403"),
+              )),
+        ),
+        actions: <Widget>[_shoppingCartBadge()],
+      ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Color(0xff009df5),
+              Color(0xff027fc6),
+              Color(0xff027fc6),
+            ],
+          ),
+        ),
+        child: ListView(
+          children: [
+            _balance(), _search(), _content()
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget _shoppingCartBadge() {
+  return badges.Badge(
+    position: badges.BadgePosition.topEnd(top: 0, end: 3),
+    badgeAnimation: badges.BadgeAnimation.slide(
+      disappearanceFadeAnimationDuration: Duration(milliseconds: 200),
+      curve: Curves.easeInCubic,
+    ),
+    showBadge: true,
+    badgeContent: Text(
+      '3',
+      style: TextStyle(color: Colors.white),
+    ),
+    badgeStyle: badges.BadgeStyle(
+      badgeColor: Colors.red,
+    ),
+    child: IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+  );
+}
+
+
+
+Widget _balance() {
+  return Padding(
+    padding: const EdgeInsets.only(left: 12.0, right: 12),
+    child: Column(
+      children: [
+        SizedBox(
+          height: 17,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Баланс кошелька ImPay ',
+                style: TextStyle(color: Colors.white)),
+            Text(
+              '5 485,67 ₽',
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            )
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _search() {
+  return Padding(
+    padding: const EdgeInsets.only(left: 12, right: 12),
+    child: Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 12.0),
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white12,
+            suffixIcon: Icon(
+              Icons.search_outlined,
+              color: Colors.white,
+            ),
+            hintText: 'Поиск',
+            hintStyle: TextStyle(color: Colors.white),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _content() {
+  return Padding(
+    padding: const EdgeInsets.only(top: 14.0),
+    child: Container(
+      height: 700,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      child: Text('wdvd'),
+    ),
+  );
 }
