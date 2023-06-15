47751cf02b0448187d064db8eeab3ef728339d26
Илья <54807032+ElMKV@users.noreply.github.com>
2023-06-15 13:59:59 +0500

    add first screen

--- news/lib/futures/main/main.dart
+++ news/lib/futures/main/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart' as badges;
+import 'package:news/core/constant.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,24 +29,53 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   color: Colors.red,
-    //   // decoration: const BoxDecoration(
-    //   //     gradient: LinearGradient(
-    //   //       begin: Alignment.topCenter,
-    //   //       end: Alignment(1, 0.001),
-    //   //       colors: <Color>[
-    //   //         Color(0xff009df5),
-    //   //         Color(0xff027fc6),
-    //   //       ],
-    //   //       // Gradient from https://learnui.design/tools/gradient-generator.html
-    //   //       // tileMode: TileMode.mirror,
-    //   //     )),
-    //   child: Scaffold(
-
-    //       body: _body()),
-    // );
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+
+        unselectedLabelStyle: TextStyle(color: Color(0xff8a898e)),
+        showUnselectedLabels: true,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              '${AppConstants.icons}union.svg',
+              fit: BoxFit.scaleDown,
+            ),
+            label: 'Главная',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              '${AppConstants.icons}shopping_bag.svg',
+              fit: BoxFit.scaleDown,
+            ),
+            label: 'Покупки',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              '${AppConstants.icons}wallet.svg',
+              fit: BoxFit.scaleDown,
+            ),
+            label: 'Платежи',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              '${AppConstants.icons}chat.svg',
+              fit: BoxFit.scaleDown,
+            ),
+            label: 'Чат',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              '${AppConstants.icons}gifts.svg',
+              fit: BoxFit.scaleDown,
+            ),
+            label: 'Бонусы',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Colors.blue,
+        onTap: null,
+      ),
       backgroundColor: Colors.transparent,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -58,14 +89,14 @@ class MainPage extends StatelessWidget {
               child: CircleAvatar(
                 radius: 24.0,
                 backgroundImage: NetworkImage(
-                    "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403"),
+                    "https://media.istockphoto.com/id/1322123064/photo/portrait-of-an-adorable-white-cat-in-sunglasses-and-an-shirt-lies-on-a-fabric-hammock.jpg?s=612x612&w=0&k=20&c=-G6l2c4jNI0y4cenh-t3qxvIQzVCOqOYZNvrRA7ZU5o="),
               )),
         ),
-        actions: <Widget>[_shoppingCartBadge()],
+        actions: <Widget>[_notification()],
       ),
       extendBodyBehindAppBar: true,
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.centerRight,
@@ -77,50 +108,46 @@ class MainPage extends StatelessWidget {
           ),
         ),
         child: ListView(
-          children: [
-            _balance(), _search(), _content()
-          ],
+          children: [_balance(), _search(), _content()],
         ),
       ),
     );
   }
 }
 
-Widget _shoppingCartBadge() {
+Widget _notification() {
   return badges.Badge(
     position: badges.BadgePosition.topEnd(top: 0, end: 3),
-    badgeAnimation: badges.BadgeAnimation.slide(
+    badgeAnimation: const badges.BadgeAnimation.slide(
       disappearanceFadeAnimationDuration: Duration(milliseconds: 200),
       curve: Curves.easeInCubic,
     ),
     showBadge: true,
-    badgeContent: Text(
+    badgeContent: const Text(
       '3',
       style: TextStyle(color: Colors.white),
     ),
-    badgeStyle: badges.BadgeStyle(
+    badgeStyle: const badges.BadgeStyle(
       badgeColor: Colors.red,
     ),
-    child: IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+    child: IconButton(icon: const Icon(Icons.notifications), onPressed: () {}),
   );
 }
-
-
 
 Widget _balance() {
   return Padding(
     padding: const EdgeInsets.only(left: 12.0, right: 12),
     child: Column(
       children: [
-        SizedBox(
+        const SizedBox(
           height: 17,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Баланс кошелька ImPay ',
+            const Text('Баланс кошелька ImPay ',
                 style: TextStyle(color: Colors.white)),
-            Text(
+            const Text(
               '5 485,67 ₽',
               style:
                   TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
@@ -136,11 +163,11 @@ Widget _search() {
   return Padding(
     padding: const EdgeInsets.only(left: 12, right: 12),
     child: Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         shape: BoxShape.rectangle,
       ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 12.0),
+      child: const Padding(
+        padding: EdgeInsets.only(top: 12.0),
         child: TextField(
           decoration: InputDecoration(
             filled: true,
@@ -164,11 +191,229 @@ Widget _content() {
     padding: const EdgeInsets.only(top: 14.0),
     child: Container(
       height: 700,
-      decoration: BoxDecoration(
-        color: Colors.white,
+      decoration: const BoxDecoration(
+        color: Color(0xfff4f5fa),
         borderRadius: BorderRadius.all(Radius.circular(15)),
       ),
-      child: Text('wdvd'),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 27.0, left: 12, right: 12),
+        child: Container(
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Избранное'.toUpperCase(),
+                    style: const TextStyle(color: Color(0xff8a898e)),
+                  )),
+              _favoriteView(),
+              _newsView()
+            ],
+          ),
+        ),
+      ),
     ),
   );
+}
+
+Widget _favoriteView() {
+  List<String> title = [
+    'Мои платежи',
+    'Билеты',
+    'Карты лояльности',
+    'QR - оплата'
+  ];
+  List<Widget> assets = [
+    SvgPicture.asset('${AppConstants.icons}my_payments.svg',
+        fit: BoxFit.scaleDown),
+    SvgPicture.asset(
+      '${AppConstants.icons}coupon.svg',
+      fit: BoxFit.scaleDown,
+    ),
+    SvgPicture.asset(
+      '${AppConstants.icons}park_tickets_couple.svg',
+      fit: BoxFit.scaleDown,
+    ),
+    SvgPicture.asset(
+      '${AppConstants.icons}qr_menu.svg',
+      fit: BoxFit.scaleDown,
+    ),
+  ];
+  return Container(
+    height: 130,
+    child: ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: title.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+            elevation: 2.1,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: SizedBox(
+                  width: 84,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Align(alignment: Alignment.topLeft, child: assets[index]),
+                      Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(title[index])),
+                    ],
+                  )),
+            ));
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const SizedBox(
+          width: 16,
+        );
+      },
+    ),
+  );
+}
+
+Widget _newsView() {
+  List<String> urlImg = [
+    'https://rtvi.com/wp-content/uploads/2022/04/ecf4cf42984f6a01289206ef6d915adc-1.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8H4C_ReoC7hA7Gfv8T6sj0i6j0eSoZTlJAA&usqp=CAU'
+  ];
+  List<String> textTitle = [
+    'Суперакция от Веккер Закажи окно до конца сентября и получи мегаскидку плюсь бонусы на счёт.',
+    'При заказе одной кружки кофе Вы получите 20 бонусов на счет.'
+  ];
+  return Column(
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Новости'.toUpperCase(),
+              style: const TextStyle(color: Color(0xff8a898e))),
+          SvgPicture.asset('${AppConstants.icons}back.svg',
+              fit: BoxFit.scaleDown),
+        ],
+      ),
+      const SizedBox(
+        height: 25,
+      ),
+      SizedBox(
+        width: 500,
+        height: 180,
+        child: Center(
+          child: ListView.separated(
+            shrinkWrap: true,
+            itemCount: urlImg.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (BuildContext context, int index) {
+              return ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: InkWell(
+                    onTap: () {},
+                    splashColor: Colors.blue,
+                    child: Container(
+                      width: 180,
+                      height: 180,
+                      alignment: Alignment.bottomCenter,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            urlImg[index],
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Align(
+                          alignment: AlignmentDirectional.bottomCenter,
+                          child: Text(
+                            textTitle[index],
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ));
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(
+                width: 14,
+              );
+            },
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
 }
